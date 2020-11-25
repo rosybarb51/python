8323949d1187436df0609436ef126d0a4098f9a1
rosybarb51 <liloding@naver.com>
2020-11-25 12:25:22 +0900

    홀수문제 수정

--- 20201125_quest02_holsu_me.py
+++ 20201125_quest02_holsu_me.py
@@ -1,4 +1,12 @@
+##문제2) 함수 is_odd(number) 생성
+## - 짝수이면 True 리턴
+## - 홀수이면 False 리턴
+##반복문
+## exit EXIT quit 눌렀을 때는 종료
+
 is_odd = int(input("짝수, 홀수를 판별할 숫자를 입력해주세요 : "))
 if is_odd % 2 == 0 :
     print ("True")
 else : print ("False")
+
+
