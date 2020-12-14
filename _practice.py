2bfe1b7b7b8fec8affc4a7debb035fbaded2c3c1
rosybarb51 <liloding@naver.com>
2020-12-14 16:28:45 +0900

    2020년 12월 14일 수업 내용

--- _practice.py
+++ _practice.py
@@ -1,16 +1,17 @@
 def is_odd(number):
-    if int(number) % 2 == 0 :
+    if int(number) %2 == 0:
         return True
-    else :
+    else:
         return False
 while True :
-    number = input("숫자를 입력하세요 : ")
-    if number in ['quit','exit']:
-        print("프로그램을 종료합니다.")
+    number = input("숫자 입력: ")
+
+    if number in ['exit','quit']:
+        print("프로그램 종료")
         break
 
     result = is_odd(number)
-    if result == True :
-        print("짝수입니다.")
-    else :
-        print("홀수입니다.")
+
+    if result == True : print("짝수")
+    else : print("홀수")
+    
